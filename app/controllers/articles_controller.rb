32dbe5f92d19a6945c6ccc4e96ad498657f9aff5
huro3h_satoshi <huro3h@gmail.com>
2017-02-21 23:00:09 +0900

    Update rspec model test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,7 @@ class ArticlesController < ApplicationController
 
   def show
   end
+
+  def new
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Article, type: :model do
+describe Article do
+  it 'タイトルとテーマがあれば有効な状態であること' do
+  end
+
+  it 'タイトルがなければ無効な状態であること' do
+  end
+
+  it 'テーマがなければ無効な状態であること' do
+  end
+
+  it '重複したタイトルなら無効な状態であること' do
+  end
 end
