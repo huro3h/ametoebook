eb76f8ec0f329d6a89dbc491a57ce4038291affc
huro3h_satoshi <huro3h@gmail.com>
2017-02-22 00:12:04 +0900

    Update rspec test about Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 describe Article do
   it 'タイトルとテーマがあれば有効な状態であること' do
+    article = Article.new(
+      title: "今日の出来事",
+      theme: "日記"
+      )
+    expect(article).to be_valid
   end
 
   it 'タイトルがなければ無効な状態であること' do
+    article = Article.new(title: nil)
+    article.valid?
+    expect(article.errors[:title]).to include("can't be blank")
   end
 
   it 'テーマがなければ無効な状態であること' do
