6abad16dcb14944160d6a254b2629819f7cc845c
huro3h_satoshi <huro3h@gmail.com>
2017-02-28 20:59:26 +0900

    Update article_spec status:red

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,8 +4,7 @@ describe Article do
   it 'タイトルとテーマがあれば有効な状態であること' do
     article = Article.new(
       title: "今日の出来事",
-      theme: "日記"
-      )
+      theme: "日記")
     expect(article).to be_valid
   end
 
@@ -16,9 +15,23 @@ describe Article do
   end
 
   it 'テーマがなければ無効な状態であること' do
+    article = Article.new(theme: nil)
+    article.valid?
+    expect(article.errors[:theme]).to include("can't be blank")
   end
 
   it '重複したタイトルなら無効な状態であること' do
+    Article.create(
+      title: "ああああ",
+      theme: "日記"
+      )
+
+    article = Article.new(
+      title: "ああああ",
+      theme: "雑談"
+      )
+     article.valid?
+     expect(article.errors[:title]).to include("has already been taken")
   end
 end
 
