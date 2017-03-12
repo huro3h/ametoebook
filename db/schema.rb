f5959e82ca75bcd39a9d2c3fe8a7d1ce6a2600a6
huro3h_satoshi <huro3h@gmail.com>
2017-03-12 16:14:54 +0900

    Add column url to article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104000912) do
+ActiveRecord::Schema.define(version: 20170312070718) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170104000912) do
     t.text     "detail"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "url"
   end
 
 end
