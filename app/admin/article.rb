3586d7ebace750b049ffbe179e9caf433743b534
huro3h_satoshi <huro3h@gmail.com>
2017-03-21 00:30:37 +0900

    Update admin/article for activeadmin

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Article do
   permit_params :title, :writing_date, :theme, :detail, :url
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :theme
+    column :url
+    column :writing_date
+    actions
+  end
 end
