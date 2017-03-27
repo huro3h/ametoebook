3b89fc028a50d55ce1aee418431f4284fb16920b
huro3h_satoshi <huro3h@gmail.com>
2017-03-27 23:15:09 +0900

    Fix article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,14 +14,12 @@ class Article < ApplicationRecord
     url = 'http://ameblo.jp/sakenomi1730/'
     html = open(url) { |f| f.read }
     doc = Nokogiri::HTML.parse(html, nil)
-    # p doc
     doc.css(".js-entryWrapper").each do |entry|
       article.title = entry.css('h1').text.to_s.gsub(/(\s)/,"")
       article.url = entry.css('a')[0][:href].to_s
       article.writing_date = entry.css('time').text.to_datetime
       article.detail = entry.css('div.articleText').text.gsub(/(\s)/,"")
       article.theme = entry.css('span.articleTheme').text.gsub(/テーマ：/,"")
-      # binding.pry
       article.save
     end
   end
