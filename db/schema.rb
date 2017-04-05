79d41635dea7833c2bec1f708b2a4fdf65336402
huro3h_satoshi <huro3h@gmail.com>
2017-04-05 21:31:20 +0900

    Add comment model belongs to article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402123728) do
+ActiveRecord::Schema.define(version: 20170405111619) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 20170402123728) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "mark"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :comment do
+    detail "MyText"
+  end
+end
