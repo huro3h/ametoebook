ae4607363d52dc2f91faeb189b8a57464a5e9a86
huro3h_satoshi <huro3h@gmail.com>
2017-09-06 14:56:37 +0900

    Update files about article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,11 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    @comment  = @article.comments.build
+    # @comment  = @article.comments.build
   end
 
   def new
+    urls = Article.new
+    urls.get_urls
   end
 end
